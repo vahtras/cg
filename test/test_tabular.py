3accb226749b3e40a725b2e9331b794c293544b2
Olav Vahtras <vahtras@kth.se>
2013-02-19 14:07:57 +0100

    Including t*t test

--- test/test_tabular.py
+++ test/test_tabular.py
@@ -50,5 +50,26 @@ def test_ts():
     ts = cg.CG(1, 0, 1)
     assert_cg(ref, ts)
 
+def test_tts():
+    isq3 = sqrt(1.0/3.0)
+    ref = [[0., 0., isq3], [0., -isq3, 0.], [isq3, 0., 0.]]
+    tts = cg.CG(1, 1, 0)
+    assert_cg(ref, tts)
+
+def test_ttt():
+    isq2 = sqrt(1.0/2.0)
+    isq3 = sqrt(1.0/3.0)
+    ref = [[0., -isq2, -isq2], [isq2, 0., -isq2], [isq2, isq2, 0.]]
+    ttt = cg.CG(1, 1, 1)
+    assert_cg(ref, ttt)
+
+def test_ttq():
+    isq2 = sqrt(1.0/2.0)
+    isq3 = sqrt(1.0/3.0)
+    isq6 = sqrt(1.0/6.0)
+    ref = [[1., isq2, isq6], [isq2, 2*isq6, isq2], [isq6, isq2, 1.]]
+    ttq = cg.CG(1, 1, 2)
+    assert_cg(ref, ttq)
+
 if __name__ == "__main__":
     test_dds()
